71cc8544a9e320ad6c188a19f3a4d22c1551ea75
winqdev <103835411+winqdev@users.noreply.github.com>
2023-08-06 04:32:39 +0300

    uploaded lol

---
+++ script.js
@@ -0,0 +1,5 @@
+//alert('Welcome to Voices of The Void game archive website, please take into consideration that mobile devices may be incompatible with that page, website still have lotta of bugs, so please be patient and wait for new updates :)')
+
+function download(version) {
+    location.href = `https://download.votv.fun/download/${version}`
+}
