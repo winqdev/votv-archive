54ec7e631d0ade3307306076eaa9ad14eb7cd8b5
winqdev <103835411+winqdev@users.noreply.github.com>
2023-08-06 12:55:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//alert('Welcome to Voices of The Void game archive website, please take into consideration that mobile devices may be incompatible with that page, website still have lotta of bugs, so please be patient and wait for new updates :)')
+alert('Welcome to Voices of The Void game archive website, please take into consideration that mobile devices may be incompatible with that page, website still have lotta of bugs, so please be patient and wait for new updates :)')
 
 function download(version) {
     location.href = `https://download.votv.fun/download/${version}`
